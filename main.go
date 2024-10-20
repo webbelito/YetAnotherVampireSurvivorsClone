cb646e429cba9037c056a90ef82006c414f87c2a
webbelito <xeeero@gmail.com>
2024-10-20 16:11:20 +0200

    #2 - Completed Initialize Game Window and Basic Loop

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"fmt"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const SCREEN_WIDTH = 1920
+const SCREEN_HEIGHT = 1080
+const SCREEN_TITLE = "YAVSC - Yet Another Vampire Survivors Clone"
+
 func main() {
 
-	rl.InitWindow(800, 450, "YAVSC - Yet Another Vampire Survivors Clone")
+	rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-	fmt.Println("Hello, World!")
+	rl.SetTargetFPS(144)
+
+	for !rl.WindowShouldClose() {
+
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawText(SCREEN_TITLE, SCREEN_WIDTH/2-500, SCREEN_HEIGHT/2, 48, rl.Maroon)
+
+		rl.EndDrawing()
+	}
 }
